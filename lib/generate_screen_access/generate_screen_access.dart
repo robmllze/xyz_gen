df5ce2638f426755ee03b46a0a4ff220139ec6c2
Robert Mollentze <robmllze@gmail.com>
2023-08-31 11:43:51 +1200

    update

--- lib/generate_screen_access/generate_screen_access.dart
+++ lib/generate_screen_access/generate_screen_access.dart
@@ -72,8 +72,9 @@ Future<void> generateScreenAccess({
     "___PATHS_ACCESSIBLE_ONLY_IF_SIGNED_OUT___": f,
   });
   await writeFile(outputFilePath, outputContent);
+  final joined = sorted.map((e) => "`$e`").joinWithLastSeparator(lastSeparator: " and ");
   printGreen(
-    "Generated screen access for `$sorted` in `${getBaseName(outputFilePath)}`",
+    "Generated screen access for $joined in `${getBaseName(outputFilePath)}`",
   );
 }
 
