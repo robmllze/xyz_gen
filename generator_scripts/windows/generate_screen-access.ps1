8c1e902006e53c314d9502dc616ab76722b307a7
Robert Mollentze <robmllze@gmail.com>
2023-09-01 15:24:46 +1200

    update

--- generator_scripts/windows/generate_screen-access.ps1
+++
@@ -1,8 +0,0 @@
-# Include the paths file.
-. .\_paths.ps1
-
-# Generate using the default arguments.
-dart ..\src\generate_screen_access.dart -r "$LIB_PATH:$SHARED_LIB_PATH" -s "screens" -o "$SCREENS_PATH\screen_access_g.dart" -t $SCREEN_ACCESS_TEMPLATE_PATH
-
-# This will also generate using the default arguments.
-#dart ..\src\generate_screen_access.dart
