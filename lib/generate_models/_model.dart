15b20e000c97efe0d1a27dcb2f525bb978835b67
robmllze <86869208+robmllze@users.noreply.github.com>
2023-10-26 15:14:47 +1100

    update

--- lib/generate_models/_model.dart
+++ lib/generate_models/_model.dart
@@ -19,10 +19,10 @@ abstract class Model {
 
   String getCollectionPath() => _removeLastSegment(this.getDocPath());
 
-  JMap toJMap({dynamic defaultValue});
+  JMap toJMap({dynamic defaultValue, bool includeNulls = false});
 
-  JMap sortedJMap() {
-    final a = this.toJMap().nonNulls;
+  JMap sortedJMap({dynamic defaultValue, bool includeNulls = false}) {
+    final a = this.toJMap(defaultValue: defaultValue, includeNulls: includeNulls);
     final b = a.keys.toList(growable: false)..sort((k1, k2) => k1.compareTo(k2));
     final c = {for (var k in b) k: a[k] as dynamic};
     return c;
