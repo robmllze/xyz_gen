7463dfdeff491aa08822e06a5949377622c242dd
robmllze <86869208+robmllze@users.noreply.github.com>
2023-11-14 11:32:27 +1100

    update

--- lib/get_xyz_gen_lib_path.dart
+++ lib/get_xyz_gen_lib_path.dart
@@ -6,11 +6,29 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
 import 'package:xyz_utils/xyz_utils_non_web.dart' show getPackageLibPath;
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-/// Returns the path of `lib` directory of the `xyz_gen` package.
+/// Returns the full path of the `xyz_gen\lib` directory.
 Future<String> getXyzGenLibPath() async {
   return (await getPackageLibPath("xyz_gen"))!;
+}
+
+/// Returns the path of the default template file with the given [templateName].
+Future<String> getXyzGenDefaultTemplateFilePath(String templateName) async {
+  final libPath = await getXyzGenLibPath();
+  final templatePath = p.join(libPath, "templates", templateName);
+  return templatePath;
+}
+
+/// Returns the content of the default template file with the given [templateName].
+Future<String> getXyzGenDefaultTemplate(String templateName) async {
+  final templatePath = await getXyzGenDefaultTemplateFilePath(templateName);
+  final file = File(templatePath);
+  final content = await file.readAsString();
+  return content;
 }
